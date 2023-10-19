5fd06ef370e86c8aba5a6bbc0f3a3b451bc8fe6e
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-10-19 12:37:48 -0300

    feat: add robotron image component with color change handle

---
+++ src/components/ChangeRobotButton.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+interface ColorButtonProps {
+	color: string;
+	onClick: () => void;
+}
+
+export default function ChangeRobotButton({ color, onClick }: ColorButtonProps) {
+	return (
+		<>
+			<button
+				className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5 w-21'
+				onClick={onClick}
+			>
+				{color}
+			</button>
+		</>
+	);
+}

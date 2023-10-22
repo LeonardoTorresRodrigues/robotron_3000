31f8a089a85e58fefc188212545a6a330ab299b1
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-10-22 13:22:51 -0300

    style: add new background image and change h2 style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,22 @@ export default function Home() {
 
 	return (
 		<>
-			<main className="flex min-h-screen flex-col items-center justify-between p-24 text-center">
+			<main className="flex min-h-screen flex-col items-center justify-between p-24 text-center bg-[url('/img/fundo.jpg')] bg-cover">
 				<img
 					className='w-96'
-					src={`/img/Robotron 3000 - ${image || 'Padrao'}.png`}
+					src={`/img/Robotron 3000 - ${image || 'Azul'}.png`}
 					alt="Robotron"
 				/>
-				<h2 className='m-8 text-2xl'>Trocar cor do seu Robotron</h2>
-				<div className='grid sm:grid-cols-1 md:grid-cols-3 text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>
-					<ChangeRobotButton color='Azul' onClick={() => handleImage('Azul')} />
-					<ChangeRobotButton color='Amarelo' onClick={() => handleImage('Amarelo')} />
-					<ChangeRobotButton color='Branco' onClick={() => handleImage('Branco')} />
-					<ChangeRobotButton color='Preto' onClick={() => handleImage('Preto')} />
-					<ChangeRobotButton color='Rosa' onClick={() => handleImage('Rosa')} />
-					<ChangeRobotButton color='Vermelho' onClick={() => handleImage('Vermelho')} />
+				<div>
+					<h2 className='m-8 text-8xl text-white font-bold py-2 px-4 bg-blue-500 rounded m-5 w-21'>Trocar cor do seu Robotron</h2>
+					<div className='grid sm:grid-cols-1 md:grid-cols-3 text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>
+						<ChangeRobotButton color='Azul' onClick={() => handleImage('Azul')} />
+						<ChangeRobotButton color='Amarelo' onClick={() => handleImage('Amarelo')} />
+						<ChangeRobotButton color='Branco' onClick={() => handleImage('Branco')} />
+						<ChangeRobotButton color='Preto' onClick={() => handleImage('Preto')} />
+						<ChangeRobotButton color='Rosa' onClick={() => handleImage('Rosa')} />
+						<ChangeRobotButton color='Vermelho' onClick={() => handleImage('Vermelho')} />
+					</div>
 				</div>
 			</main>
 		</>

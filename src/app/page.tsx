89313f9fa40947e62265d420334de037376cfc17
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-10-22 13:24:41 -0300

    fix: margin on h2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 					alt="Robotron"
 				/>
 				<div>
-					<h2 className='m-8 text-8xl text-white font-bold py-2 px-4 bg-blue-500 rounded m-5 w-21'>Trocar cor do seu Robotron</h2>
+					<h2 className='text-8xl text-white font-bold py-2 px-4 bg-blue-500 rounded m-5 w-21'>Trocar cor do seu Robotron</h2>
 					<div className='grid sm:grid-cols-1 md:grid-cols-3 text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>
 						<ChangeRobotButton color='Azul' onClick={() => handleImage('Azul')} />
 						<ChangeRobotButton color='Amarelo' onClick={() => handleImage('Amarelo')} />

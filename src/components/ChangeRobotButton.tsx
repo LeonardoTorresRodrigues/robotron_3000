4f8201f3cbeb29a894da1dcca973369490c54d0d
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-10-25 13:22:59 -0300

    refactor: change background color

--- src/components/ChangeRobotButton.tsx
+++ src/components/ChangeRobotButton.tsx
@@ -9,7 +9,7 @@ export default function ChangeRobotButton({ color, onClick }: ColorButtonProps) 
 	return (
 		<>
 			<button
-				className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5 w-21'
+				className='bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5 w-21'
 				onClick={onClick}
 			>
 				{color}

ed553f5d60a73a0355c52226239ff0a812a023f5
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-10-25 13:22:17 -0300

    feat: add modal to change image color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,30 +5,55 @@ import ChangeRobotButton from '@/components/ChangeRobotButton';
 
 export default function Home() {
 	const [image, setImage] = useState<string>('');
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	const handleImage = (color: string) => {
 		setImage(color);
+		setIsModalOpen(false);
 	};
 
 	return (
 		<>
 			<main className="flex min-h-screen flex-col items-center justify-between p-24 text-center bg-[url('/img/fundo.jpg')] bg-cover">
-				<img
-					className='w-96'
-					src={`/img/Robotron 3000 - ${image || 'Azul'}.png`}
-					alt="Robotron"
-				/>
-				<div>
-					<h2 className='text-8xl text-white font-bold py-2 px-4 bg-blue-500 rounded m-5 w-21'>Trocar cor do seu Robotron</h2>
-					<div className='grid sm:grid-cols-1 md:grid-cols-3 text-md sm:text-lg md:text-xl lg:text-2xl xl:text-3xl'>
-						<ChangeRobotButton color='Azul' onClick={() => handleImage('Azul')} />
-						<ChangeRobotButton color='Amarelo' onClick={() => handleImage('Amarelo')} />
-						<ChangeRobotButton color='Branco' onClick={() => handleImage('Branco')} />
-						<ChangeRobotButton color='Preto' onClick={() => handleImage('Preto')} />
-						<ChangeRobotButton color='Rosa' onClick={() => handleImage('Rosa')} />
-						<ChangeRobotButton color='Vermelho' onClick={() => handleImage('Vermelho')} />
-					</div>
+				<div className='relative'>
+					<button
+						onClick={() => setIsModalOpen(true)}
+						className='hidden md:block fixed left-0 top-1/2 transform -translate-y-1/2 px-4 py-2 bg-blue-400 text-white font-bold rounded'
+					>
+						Trocar cor do seu Robotron
+					</button>
+					<img
+						className='w-96 md:mx-auto'
+						src={`/img/Robotron 3000 - ${image || 'Azul'}.png`}
+						alt="Robotron"
+					/>
+
+					<button
+						onClick={() => setIsModalOpen(true)}
+						className="md:hidden block mx-auto mt-4 px-4 py-2 bg-blue-400 text-white font-bold rounded"
+					>
+						Trocar cor do seu Robotron
+					</button>
 				</div>
+
+				{isModalOpen && (
+					<div
+						className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 z-50 flex items-center justify-center"
+						onClick={() => setIsModalOpen(false)}
+					>
+						<div className="bg-white p-8 rounded shadow-lg" onClick={(e) => e.stopPropagation()}>
+							<h2 className="text-4xl font-bold mb-4 text-blue-400">Escolha uma cor</h2>
+							<div className="grid grid-cols-3 gap-4">
+								<ChangeRobotButton color="Azul" onClick={() => handleImage('Azul')} />
+								<ChangeRobotButton color="Amarelo" onClick={() => handleImage('Amarelo')} />
+								<ChangeRobotButton color="Branco" onClick={() => handleImage('Branco')} />
+								<ChangeRobotButton color="Preto" onClick={() => handleImage('Preto')} />
+								<ChangeRobotButton color="Rosa" onClick={() => handleImage('Rosa')} />
+								<ChangeRobotButton color="Vermelho" onClick={() => handleImage('Vermelho')} />
+							</div>
+						</div>
+					</div>
+				)}
 			</main>
 		</>
 	);

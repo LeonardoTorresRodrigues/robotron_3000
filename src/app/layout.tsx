035b04a7675c0d782908aa9228fb94c2a00aed95
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-11-02 23:04:21 -0300

    fix: favicon not appearing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import React from 'react';
+import Favicon from './favicon.ico';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Robotron 3000',
 	description: 'Crie seu robô de batalha!',
+	icons: [{ rel: 'icon', url: Favicon.src }]
 };
 
 export default function RootLayout({

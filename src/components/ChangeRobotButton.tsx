c6c26014798b2b5fab34f2c7b0a7b9b2cf991f7e
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2024-01-10 20:01:37 -0300

    refactor: new modal

--- src/components/ChangeRobotButton.tsx
+++ src/components/ChangeRobotButton.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
 interface ColorButtonProps {
-	className: string;
-	onClick: () => void;
+  className: string;
+  isOpen: boolean;
+  onClick: () => void;
 }
 
-export default function ChangeRobotButton({ className, onClick }: ColorButtonProps) {
-	return (
-		<>
-			<button
-				className={`text-white font-bold py-2 px-4 rounded m-5 w-21 ${className}`}
-				onClick={onClick}
-			>
-			</button>
-		</>
-	);
+export default function ChangeRobotButton({
+  className,
+  onClick,
+}: ColorButtonProps) {
+  return (
+    <>
+      <button
+        className={`text-white font-bold py-2 px-4 rounded m-5 w-21 ${className}`}
+        onClick={onClick}
+      ></button>
+    </>
+  );
 }
